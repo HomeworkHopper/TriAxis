465d1540c0017d21df8021293c4bb9f811c39fdd
Shaun Thornton <sdt8987@rit.edu>
2024-04-01 23:16:45 -0400

    MPU ISRs implemented

--- TriAxis.ino
+++ TriAxis.ino
@@ -8,7 +8,7 @@
 // MPU_C_INT  > |D3    D10| < SPI_MOSI  
 // MPU_A_EN   > |D4     D9| < SPI_MISO  
 // MPU_B_EN   > |D5     D8| < SPI_CLK  
-// MPU_C_EN   > |D6     D7| < STATUS_LED (NPN + PNP resistor)
+// MPU_C_EN   > |D6     D7| < STATUS_LED (NPN + PNP transistor)
 //               ‾‾‾‾‾‾‾‾‾
 
 #define BAT_CHARGE_PIN D0    // Battery Analog Read
@@ -20,80 +20,65 @@
 #define MPU_C_EN_PIN   D6    // MPU C Enable
 #define STATUS_LED_PIN D7    // Status LED
 
+#define MPU_SAMPLE_RATE_HZ 100
+
+typedef struct { float x; float y; float z; } component_t;
+typedef struct {
+  component_t accel;
+  component_t gyro;
+  component_t mag;
+  float temp;
+} mpu_capture_t;
+
 bfs::Mpu9250 mpu_a(&SPI, MPU_A_EN_PIN);    // MPU A
 bfs::Mpu9250 mpu_b(&SPI, MPU_B_EN_PIN);    // MPU B
 bfs::Mpu9250 mpu_c(&SPI, MPU_C_EN_PIN);    // MPU C
 
-typedef enum { STARTING, RUNNING, SLEEPING, ERROR } SYSTEM_STATE;
-volatile RTC_DATA_ATTR SYSTEM_STATE system_state = STARTING;    // System State
+mpu_capture_t* mpu_a_capture = NULL;
+mpu_capture_t* mpu_b_capture = NULL;
+mpu_capture_t* mpu_c_capture = NULL;
 
-void MPU_A_ISR() {}
-void MPU_B_ISR() {}
-void MPU_C_ISR() {}
-
-bool configureMPU(bfs::Mpu9250 mpu) {
-  return mpu.Begin()        // Establish communication with MPU
-    && mpu.ConfigSrd(19)    // Set MPU sample rate
-    && mpu.EnableDrdyInt(); // Enable data ready interrupts
+void mpu_read(bfs::Mpu9250 mpu, mpu_capture_t* ret) {
+  if (mpu.Read()) {
+    ret->accel.x = mpu.accel_x_mps2();
+    ret->accel.y = mpu.accel_y_mps2();
+    ret->accel.z = mpu.accel_z_mps2();
+    ret->gyro.x = mpu.gyro_x_radps();
+    ret->gyro.y = mpu.gyro_y_radps();
+    ret->gyro.z = mpu.gyro_z_radps();
+    ret->mag.x = mpu.mag_x_ut();
+    ret->mag.y = mpu.mag_y_ut();
+    ret->mag.z = mpu.mag_z_ut();
+    ret->temp = mpu.die_temp_c();
+  }
 }
 
-SYSTEM_STATE processStateRunning() {
+void MPU_A_ISR() { mpu_read(mpu_a, mpu_a_capture); }
+void MPU_B_ISR() { mpu_read(mpu_b, mpu_b_capture); }
+void MPU_C_ISR() { mpu_read(mpu_c, mpu_c_capture); }
+
+bool configureMPU(bfs::Mpu9250 mpu, uint8_t pin, void (*isr)()) {
+  static constexpr unsigned int mpu_srd = ((1000 - MPU_SAMPLE_RATE_HZ) / MPU_SAMPLE_RATE_HZ);
+  if(mpu.Begin() && mpu.ConfigSrd(mpu_srd) && mpu.EnableDrdyInt()) {
+    attachInterrupt(pin, isr, RISING);
+    return true;
+  }
+  return false;
 }
-
-SYSTEM_STATE processStateSleeping() {
-
-  noInterrupts();
-  // esp_sleep_enable_ext1_wakeup(ext_wakeup_pin_1_mask | ext_wakeup_pin_2_mask, ESP_EXT1_WAKEUP_ANY_HIGH);
-  esp_deep_sleep_enable_gpio_wakeup(BIT(MPU_A_INT_PIN), ESP_GPIO_WAKEUP_GPIO_HIGH);
-  esp_deep_sleep_start();    // Execution will stop here
-  
-  return RUNNING;
-}
-
-// SYSTEM_STATE (*)()
 
 void setup() {
-
-  struct timeval now;
-    gettimeofday(&now, NULL);
-    int sleep_time_ms = (now.tv_sec - sleep_enter_time.tv_sec) * 1000 + (now.tv_usec - sleep_enter_time.tv_usec) / 1000;
-
-
-  if(system_state == SLEEPING) {
-
-  } else {
-
-  }
   
-  // Initialize serial
   Serial.begin(115200);
   while(!Serial) {}
 
-  // Start the SPI bus
   SPI.begin();
 
   noInterrupts();
-  if(configureMPU(mpu_a) && configureMPU(mpu_b) && configureMPU(mpu_c)) {
-    system_state = RUNNING;
-  } else {
+  if(!configureMPU(mpu_a, MPU_A_INT_PIN, MPU_A_ISR) || !configureMPU(mpu_b, MPU_B_INT_PIN, MPU_B_ISR) || !configureMPU(mpu_c, MPU_C_INT_PIN, MPU_C_ISR)) {
     Serial.println("Unable to configure the MPUs");
-    system_state = ERROR;
   }
+  interrupts();
 }
 
 void loop() {
-  switch(system_state) {
-    case RUNNING:
-      system_state = processStateRunning();
-      break;
-    case SLEEPING:
-      system_state = processStateSleeping();
-      break;
-    case ERROR:
-      
-      while(true) ;
-    default:
-      system_state = ERROR;
-      break;
-  }
 }
