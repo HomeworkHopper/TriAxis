ab15de1055443671d5f79ca2077e3b9717cd55ac
Shaun Thornton <homeworkhopper@gmail.com>
2024-04-07 02:13:17 -0400

    Using MPU6050s now

--- TriAxis.ino
+++ TriAxis.ino
@@ -1,44 +1,32 @@
 #include <SPI.h>
 #include <Adafruit_SH110X.h>
-#include "libraries/invensense-imu/src/mpu9250.h"
+#include <Adafruit_MPU6050.h>
 
 // System pinout (final)
 //               _________
 // BAT_CHARGE > |D0     5v|
 // MPU_A_INT  > |D1    GND|
 // MPU_B_INT  > |D2    3v3|
-// MPU_C_INT  > |D3    D10| < SPI_MOSI
-// MPU_A_EN   > |D4     D9| < SPI_MISO & OLED_DC
-// MPU_B_EN   > |D5     D8| < SPI_CLK
-// MPU_C_EN   > |D6     D7| < OLED_EN
+//            > |D3    D10| < MOSI (SPI)
+// SDA (I2C)  > |D4     D9| < MISO (SPI, not used by OLED)
+// SCL (I2C)  > |D5     D8| < CLK (SPI)
+// OLED_DC    > |D6     D7| < OLED_EN
 //               ‾‾‾‾‾‾‾‾‾
 // Note: Attach OLED RST pin to the EN pin of the XIAO
 
 #define BAT_CHARGE_PIN D0    // Battery Analog Read
 #define MPU_A_INT_PIN  D1    // MPU A Interrupt
 #define MPU_B_INT_PIN  D2    // MPU B Interrupt
-#define MPU_C_INT_PIN  D3    // MPU C Interrupt
-#define MPU_A_EN_PIN   D4    // MPU A Enable
-#define MPU_B_EN_PIN   D5    // MPU B Enable
-#define MPU_C_EN_PIN   D6    // MPU C Enable
-#define OLED_EN_PIN    D7    // OLED Enable
-#define OLED_DC_PIN    D9    // OLED DC pin, NOTE: D9 is also the MISO pin, 
-                             // but the OLED display does not use the MISO pin,
-                             // so we're able to use it as the DC pin. The Adafruit
-                             // library had to be slightly modified to support this
+#define OLED_EN_PIN    D7    // OLED EN pin
+#define OLED_DC_PIN    D6    // OLED DC pin
 
-#define MPU_SAMPLE_RATE 100  // The accelerometer/gyroscope can sample at up to 1000hz (1Mhz),
-                             // but the magnometer can only run at 100hz, max. In practice,
-                             // there would be no harm in running the sensor at 1Mhz, but for
-                             // development and debugging purposes I want to keep it at 100hz
+#define MPU_SAMPLE_RATE 1000
 
 // OLED Display
-static Adafruit_SH1106G display = Adafruit_SH1106G(128, 64, &SPI, OLED_DC_PIN, -1 /*IGNORE RST PIN*/, OLED_EN_PIN);
+static Adafruit_SH1106G display = Adafruit_SH1106G(128, 64, &SPI, OLED_DC_PIN, /*IGNORE RST PIN*/ -1, OLED_EN_PIN);
 
-// MPU A, B, C
-bfs::Mpu9250 mpu_a(&SPI, MPU_A_EN_PIN);
-bfs::Mpu9250 mpu_b(&SPI, MPU_B_EN_PIN);
-bfs::Mpu9250 mpu_c(&SPI, MPU_C_EN_PIN);
+// MPU A, B
+static Adafruit_MPU6050 mpu_a, mpu_b;
 
 // MPU Data Capture Structs
 typedef struct { float x; float y; float z; } capture_component_t;
@@ -46,10 +34,10 @@ struct mpu_capture_t {
   unsigned long time;
   capture_component_t accel;
   capture_component_t gyro;
-  capture_component_t mag;
   float temp;
-} static volatile mpu_a_capture, mpu_b_capture, mpu_c_capture;
+} static volatile mpu_a_capture, mpu_b_capture;
 
+/*
 // This should probably be a macro. I need to spend some time considering if it will expand properly in all cases, though... (should there really ever be an "if" statement in a macro!?)
 static inline void mpu_capture(bfs::Mpu9250 mpu, struct mpu_capture_t volatile *capture) {
   if (mpu.Read()) { // Should always be true, since this function is invoked in reasponse to a data-ready interrupt
@@ -60,27 +48,39 @@ static inline void mpu_capture(bfs::Mpu9250 mpu, struct mpu_capture_t volatile *
     capture->gyro.x = mpu.gyro_x_radps();
     capture->gyro.y = mpu.gyro_y_radps();
     capture->gyro.z = mpu.gyro_z_radps();
-    capture->mag.x = mpu.mag_x_ut();
-    capture->mag.y = mpu.mag_y_ut();
-    capture->mag.z = mpu.mag_z_ut();
     capture->temp = mpu.die_temp_c();
   } else {
 
   }
 }
+*/
 
+/*
 void MPU_A_ISR() { mpu_capture(mpu_a, &mpu_a_capture); }
 void MPU_B_ISR() { mpu_capture(mpu_b, &mpu_b_capture); }
-void MPU_C_ISR() { mpu_capture(mpu_c, &mpu_c_capture); }
+*/
 
-bool configureMPU(bfs::Mpu9250 mpu, uint8_t interruptPin, void (*isr)()) {
-  static constexpr unsigned int mpu_srd = ((1000 - MPU_SAMPLE_RATE) / MPU_SAMPLE_RATE);
-  if(mpu.Begin() && mpu.ConfigSrd(mpu_srd) && mpu.EnableDrdyInt()) {
-    pinMode(interruptPin, INPUT);    // Default pinmode
-    attachInterrupt(interruptPin, isr, RISING);
-    return true;
+bool configureMPU(Adafruit_MPU6050 *mpu, uint8_t addr) {
+  if (!mpu->begin(addr)) {
+    Serial.println("Failed to find MPU6050 chip");
+    return false;
   }
-  return false;
+  Serial.println("MPU6050 Found!");
+
+  mpu->setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu->setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu->setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  return true;
+}
+
+
+float readBatteryVoltage() {
+  uint32_t Vbatt = 0;
+  for(int i = 0; i < 16; i++) {
+    Vbatt = Vbatt + analogReadMilliVolts(A0);  
+  }
+  return 2 * Vbatt / 16 / 1000.0;
 }
 
 // NEVER call this function directly from an IRQ! (oops)
@@ -95,43 +95,81 @@ static inline void error(char *msg) {
   while(true){}   // Loop until reset
 }
 
+static bool mpu_a_status, mpu_b_status;
+
 void setup() {
   
   Serial.begin(115200);
-  while(!Serial) {}
+  while(!Serial) { delay(10); }
 
-  SPI.begin();
+  pinMode(A0, INPUT);
 
-  noInterrupts();
-  if(!configureMPU(mpu_a, MPU_A_INT_PIN, MPU_A_ISR)) {
-    error("Unable to configure the MPUs");
-    sleep(10000);
-  }
-  interrupts();
+  mpu_a_status = configureMPU(&mpu_a, MPU6050_I2CADDR_DEFAULT);
+  mpu_b_status = configureMPU(&mpu_b, MPU6050_I2CADDR_DEFAULT + 1);
+
+  Serial.println(mpu_a_status);
+  Serial.println(mpu_b_status);
 
   // display.setContrast (0);
-  display.begin(0, true);
+  display.begin();
   display.display();
-
   delay(2000);
-  
-  display.clearDisplay();
-
-  display.setTextSize(1);
-  display.setTextColor(SH110X_WHITE);
-  display.setCursor(0, 0);
-  display.println("Failure is always an option");
-  display.setTextColor(SH110X_BLACK, SH110X_WHITE); // 'inverted' text
-  display.println(3.141592);
-  display.setTextSize(2);
-  display.setTextColor(SH110X_WHITE);
-  display.print("0x"); display.println(0xDEADBEEF, HEX);
-  display.display();
 }
+
+static sensors_event_t a, g, temp;
 
 void loop() {
   // If an error occured (probably in an ISR) then we need to stop
   if(global_error) {
-    error("a");  // Never returns
+    error("Error");  // Never returns
   }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SH110X_WHITE);
+  display.setCursor(0, 0);
+
+  display.print("Battery: ");
+  display.print(readBatteryVoltage());
+  display.println("V");
+  display.println("");
+
+  if(mpu_a_status) {
+    mpu_a.getEvent(&a, &g, &temp);
+
+    /* Print out the values */
+    display.print(a.acceleration.x);
+    display.print(", ");
+    display.print(a.acceleration.y);
+    display.print(", ");
+    display.println(a.acceleration.z);
+    display.print(g.gyro.x);
+    display.print(", ");
+    display.print(g.gyro.y);
+    display.print(", ");
+    display.println(g.gyro.z);
+
+    display.println("");
+  } else {
+    display.println("ERROR");
+  }
+
+  if(mpu_b_status) {
+    mpu_b.getEvent(&a, &g, &temp);
+    display.print(a.acceleration.x);
+    display.print(", ");
+    display.print(a.acceleration.y);
+    display.print(", ");
+    display.println(a.acceleration.z);
+    display.print(g.gyro.x);
+    display.print(", ");
+    display.print(g.gyro.y);
+    display.print(", ");
+    display.println(g.gyro.z);
+  } else {
+    display.println("ERROR");
+  }
+
+  display.display();
+  delay(500);
 }
