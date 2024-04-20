16ff188e022844f7d89dbf6e57a6d3077c5e9045
Shaun Thornton <sdt8987@rit.edu>
2024-04-20 18:19:09 -0400

    Atomic timestamp copying

--- TriAxis.ino
+++ TriAxis.ino
@@ -28,7 +28,7 @@
 #define OLED_WIDTH  128      // Width of the OLED screen, in pixels
 #define OLED_HEIGHT 64       // Height of the OLED screen, in pixels
 
-#define MPU_A_ADDR MPU6050_I2CADDR_DEFAULT      // AD0 must be pulled low (default)
+#define MPU_A_ADDR MPU6050_I2CADDR_DEFAULT      // AD0 must be pulled low (sensor has it's own pull-down)
 #define MPU_B_ADDR MPU6050_I2CADDR_DEFAULT + 1  // AD0 must be pulled high (~3.3v)
 
 #define OLED_CLEAR(oled) do {                         \
@@ -42,41 +42,46 @@
 static Adafruit_SH1106G oled_display = Adafruit_SH1106G(OLED_WIDTH, OLED_HEIGHT, &SPI, OLED_DC_PIN, /*IGNORE RST PIN*/ -1, OLED_EN_PIN);
 
 static Adafruit_MPU6050 mpu_a, mpu_b;
+
 struct triaxis_sensor_t {
   Adafruit_MPU6050 *mpuSensor;
   volatile int64_t dataTimestamp;
-  uint8_t i2cAddr;
   volatile bool dataReady;
-} static triaxis_a, triaxis_b;
+} static tri_a, tri_b;
 
-#define TRI_DATA_RDY(triaxis_sensor) triaxis_sensor.dataReady
-#define SET_TRI_DATA_RDY(triaxis_sensor) triaxis_sensor.dataTimestamp = esp_timer_get_time(); triaxis_sensor.dataReady = true;
-#define CLR_TRI_DATA_RDY(triaxis_sensor) triaxis_sensor.dataReady = false
+#define GET_TRI_DATA_RDY(tri) tri.dataReady
+#define SET_TRI_DATA_RDY(tri) ATOMIC_BLOCK { tri.dataTimestamp = esp_timer_get_time(); tri.dataReady = true; }
+#define CLR_TRI_DATA_RDY(tri) tri.dataReady = false
 
 // Interrupt Service Routines (DATA READY)
-void MPU_A_ISR() { SET_TRI_DATA_RDY(triaxis_a); }
-void MPU_B_ISR() { SET_TRI_DATA_RDY(triaxis_b); }
+void MPU_A_ISR() { SET_TRI_DATA_RDY(tri_a); }
+void MPU_B_ISR() { SET_TRI_DATA_RDY(tri_b); }
 
 static volatile bool pushbutton_interrupt;
 void PUSHBUTTON_ISR() {
   pushbutton_interrupt = true;
 }
 
-static inline bool configureTRI(struct triaxis_sensor_t *tri) {
-
-  tri->dataTimestamp = 0;
-  tri->i2cAddr = i2cAddr;
-  tri->dataReady = false;
-  tri->mpuSensor = mpu;
-
-  if (mpu->begin(i2cAddr, &Wire)) {
+static inline bool configureMPU(Adafruit_MPU6050 *mpu, const uint8_t i2cAddr) {
+  if (mpu && mpu->begin(i2cAddr, &Wire)) {
     mpu->setAccelerometerRange(MPU6050_RANGE_8_G);
     mpu->setGyroRange(MPU6050_RANGE_500_DEG);
     mpu->setFilterBandwidth(MPU6050_BAND_21_HZ);
     return true;
+  } else {
+    return false;
   }
+}
 
-  return false;
+static inline bool configureTRI(struct triaxis_sensor_t *tri, Adafruit_MPU6050 *mpu) {
+  if(mpu) {
+    tri->dataTimestamp = -1;
+    tri->dataReady = false;
+    tri->mpuSensor = mpu;
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /*
@@ -120,19 +125,20 @@ void setup() {
     error();
   }
 
-  // Configure MPU A
-  if(!configureTRI(&triaxis_a, &mpu_a, MPU_A_ADDR)) {
+  // Configure MPU/TRI A
+  if(configureMPU(&mpu_a, MPU_A_ADDR) || !configureTRI(&tri_a, &mpu_a)) {
     error(&oled_display, "Failed to configure TRI A");
   }
 
-  // Configure MPU B
-  if(!configureTRI(&triaxis_b, &mpu_b, MPU_B_ADDR)) {
+  // Configure MPU/TRI B
+  if(!configureMPU(&mpu_b, MPU_B_ADDR) || !configureTRI(&tri_b, &mpu_b)) {
     error(&oled_display, "Failed to configure TRI B");
   }
 
   // Show splash image
   oled_display.display();
 
+  // Wait for button press
   attachInterrupt(PUSHBUTTON_PIN, PUSHBUTTON_ISR, FALLING);
   for(pushbutton_interrupt = false; !pushbutton_interrupt;) {
     delay(100); // Average human button press time
@@ -150,9 +156,22 @@ void loop() {
   oled_display.print(readBatteryVoltage());
   oled_display.println("V");
 
-  if(TRI_DATA_RDY(triaxis_a)) {
-    
-    CLR_TRI_DATA_RDY(triaxis_a);
+  // Check if TRI A has new data
+  if(GET_TRI_DATA_RDY(tri_a)) {
+    int64_t capture_time;
+    ATOMIC_BLOCK {
+      capture_time = tri_a.dataTimestamp;
+      CLR_TRI_DATA_RDY(tri_a);
+    }
+  }
+
+  // Check if TRI B has new data
+  if(GET_TRI_DATA_RDY(tri_b)) {
+    int64_t capture_time;
+    ATOMIC_BLOCK {
+      capture_time = tri_b.dataTimestamp;
+      CLR_TRI_DATA_RDY(tri_b);
+    }
   }
 
   oled_display.display();
