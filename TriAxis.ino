f2df0e9716e07213b219aab1f57b97d640c1d081
Shaun Thornton <homeworkhopper@gmail.com>
2024-04-21 14:54:28 -0400

    Overhauled interrupt data-delivery system

--- TriAxis.ino
+++ TriAxis.ino
@@ -4,7 +4,7 @@
 
 #include "AtomicBlock.h"
 
-// System pinout (final)
+// System pinout
 //               _________
 // BAT_CHARGE > |D0/A0  5v|
 // MPU_A_INT  > |D1    GND|
@@ -41,22 +41,34 @@
 // OLED Display
 static Adafruit_SH1106G oled_display = Adafruit_SH1106G(OLED_WIDTH, OLED_HEIGHT, &SPI, OLED_DC_PIN, /*IGNORE RST PIN*/ -1, OLED_EN_PIN);
 
+// The MPU sensors
 static Adafruit_MPU6050 mpu_a, mpu_b;
 
+// Data capture structures
+struct triaxis_capture_t {
+  struct { float x, y, z; } accel;
+  struct { float x, y, z; } gyro;
+  int64_t dataTimestamp;
+  float temp;
+};
+
+// The TRI structures (basically just MPU sensors with packaged metadata)
 struct triaxis_sensor_t {
-  Adafruit_MPU6050 *mpuSensor;
   volatile int64_t dataTimestamp;
+  Adafruit_MPU6050 *mpuSensor;
   volatile bool dataReady;
 } static tri_a, tri_b;
 
+// TRI structure operations
 #define GET_TRI_DATA_RDY(tri) tri.dataReady
-#define SET_TRI_DATA_RDY(tri) ATOMIC_BLOCK { tri.dataTimestamp = esp_timer_get_time(); tri.dataReady = true; }
+#define SET_TRI_DATA_RDY(tri) tri.dataTimestamp = esp_timer_get_time(); tri.dataReady = true;
 #define CLR_TRI_DATA_RDY(tri) tri.dataReady = false
 
 // Interrupt Service Routines (DATA READY)
 void MPU_A_ISR() { SET_TRI_DATA_RDY(tri_a); }
 void MPU_B_ISR() { SET_TRI_DATA_RDY(tri_b); }
 
+// Push button status (and interrupt)
 static volatile bool pushbutton_interrupt;
 void PUSHBUTTON_ISR() {
   pushbutton_interrupt = true;
@@ -76,29 +88,52 @@ static inline bool configureMPU(Adafruit_MPU6050 *mpu, const uint8_t i2cAddr) {
 static inline bool configureTRI(struct triaxis_sensor_t *tri, Adafruit_MPU6050 *mpu) {
   if(mpu) {
     tri->dataTimestamp = -1;
-    tri->dataReady = false;
     tri->mpuSensor = mpu;
+    tri->dataReady = false;
     return true;
   } else {
     return false;
   }
 }
 
-/*
-static inline bool readNewMPUData() {
-  if(MPU_DATA_RDY(mpu_a_status)) {
-    CLR_MPU_DATA_RDY(mpu_a_status);
-    
-    // Read the new data
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
+static inline bool processData(const struct triaxis_sensor_t  *tri,
+    void (*processingFunction)(const struct triaxis_capture_t *capture)) {
+  
+  // Read sensor data
+  static sensors_event_t a, g, t;
+  tri->mpuSensor->getEvent(&a, &g, &t);
+
+  // Triaxis data
+  static triaxis_capture_t captureData;
+
+  // Copy timestamp (atomically)
+  ATOMIC_BLOCK {
+    captureData.dataTimestamp = tri->dataTimestamp;
   }
+
+  // Copy acceleration data
+  captureData.accel.x = a.acceleration.x;
+  captureData.accel.y = a.acceleration.y;
+  captureData.accel.z = a.acceleration.z;
+
+  // Copy gyroscope data
+  captureData.gyro.x = g.gyro.x;
+  captureData.gyro.y = g.gyro.y;
+  captureData.gyro.z = g.gyro.z;
+
+  // Copy temperature data
+  captureData.temp = t.temperature;
+
+  // Process the new data
+  processingFunction(&captureData);
 }
-*/
+
+static inline void displayCaptureDataOLED(const struct triaxis_capture_t *capture) {
+
+}
 
 static inline float readBatteryVoltage() {
   static constexpr uint32_t Vdivisor = 500 * BAT_READ_SAMPLES;
-
   uint32_t Vtotal = 0;
   for(int i = 0; i < BAT_READ_SAMPLES; i++) {
     Vtotal += analogReadMilliVolts(BAT_CHARGE_PIN);
@@ -115,6 +150,7 @@ static inline void error(Adafruit_SH1106G *oled = nullptr, const char *msg = "Ge
     _oled.display();
   }
 
+  // Never return
   while(true) { delay(10); }
 }
 
@@ -125,12 +161,12 @@ void setup() {
     error();
   }
 
-  // Configure MPU/TRI A
-  if(configureMPU(&mpu_a, MPU_A_ADDR) || !configureTRI(&tri_a, &mpu_a)) {
+  // Configure TRI A
+  if(!configureMPU(&mpu_a, MPU_A_ADDR) || !configureTRI(&tri_a, &mpu_a)) {
     error(&oled_display, "Failed to configure TRI A");
   }
 
-  // Configure MPU/TRI B
+  // Configure TRI B
   if(!configureMPU(&mpu_b, MPU_B_ADDR) || !configureTRI(&tri_b, &mpu_b)) {
     error(&oled_display, "Failed to configure TRI B");
   }
@@ -144,36 +180,34 @@ void setup() {
     delay(100); // Average human button press time
   }
 
+  // Start listening for interrupts
   attachInterrupt(MPU_A_INT_PIN, MPU_A_ISR, RISING);
   attachInterrupt(MPU_B_INT_PIN, MPU_B_ISR, RISING);
 }
 
 void loop() {
 
+  // Clear the screen
   OLED_CLEAR(oled_display);
 
+  // Print battery voltage
   oled_display.print("Battery: ");
   oled_display.print(readBatteryVoltage());
   oled_display.println("V");
 
   // Check if TRI A has new data
   if(GET_TRI_DATA_RDY(tri_a)) {
-    int64_t capture_time;
-    ATOMIC_BLOCK {
-      capture_time = tri_a.dataTimestamp;
-      CLR_TRI_DATA_RDY(tri_a);
-    }
+    processData(&tri_a, &displayCaptureDataOLED);
+    CLR_TRI_DATA_RDY(tri_a);
   }
 
   // Check if TRI B has new data
   if(GET_TRI_DATA_RDY(tri_b)) {
-    int64_t capture_time;
-    ATOMIC_BLOCK {
-      capture_time = tri_b.dataTimestamp;
-      CLR_TRI_DATA_RDY(tri_b);
-    }
+    processData(&tri_b, &displayCaptureDataOLED);
+    CLR_TRI_DATA_RDY(tri_b);
   }
 
+  // Update the display
   oled_display.display();
   delay(500);
 }
