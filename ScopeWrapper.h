ab188905e126049b00885f1e052e02634df827e3
Shaun Thornton <sdt8987@rit.edu>
2024-05-08 22:40:39 -0400

    Refactored ScopeWrapper... again

--- ScopeWrapper.h
+++ ScopeWrapper.h
@@ -1,30 +1,15 @@
 template <typename Type>
 class ScopeWrapper {
     void (*_exit_func)(Type);
-    bool _is_valid;
     Type _state;
 
 public:
     ScopeWrapper(Type (*enter_func)(), void (*exit_func)(Type)) {
         _exit_func = exit_func;
         _state = enter_func();
-        validate();
     }
 
     ~ScopeWrapper() { _exit_func(_state); }
-
-    [[nodiscard]]
-    bool is_valid() const {
-        return _is_valid;
-    }
-
-    void validate() {
-        _is_valid = true;
-    }
-
-    void invalidate() {
-        _is_valid = false;
-    }
 };
 
-#define WRAP_SCOPE(type, entr, exit) for(ScopeWrapper<type> _sw(entr, exit); _sw.is_valid(); _sw.invalidate())
+#define WRAP_SCOPE(type, entr, exit) for(struct {ScopeWrapper<type> sw; bool flag;} _scope = {ScopeWrapper<type>(entr, exit), true}; _scope.flag; _scope.flag = false)
